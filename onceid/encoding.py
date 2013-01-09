f621e1c546ccc689fb153334f4103793387f7314
Rick Yakubowski <orphasystems@scipio.local>
2013-01-09 10:43:46 -0500

    Cleanup.

--- onceid/encoding.py
+++ onceid/encoding.py
@@ -2,7 +2,8 @@ from onceid import ONCEB50, ONCESIZE
 
 # Convert a numeric ID to a OnceID.
 def toOnceID(dividend):
-  if not isinstance( dividend, (int, long) ) or dividend < 0: return None
+  if not isinstance(dividend, (int, long)) or dividend < 0:
+    return None
   if dividend == 0: return ONCEB50[0]
   once_id = ''
   # The math is done from right-to-left, so we're prepending to the OnceID.
@@ -15,7 +16,8 @@ def toOnceID(dividend):
 
 # Convert a OnceID back to an numeric ID.
 def fromOnceID(once_id):
-  if not isinstance( once_id, (str) ) or once_id == '': return None
+  if not isinstance( once_id, (str) ) or once_id == '':
+    return None
   id_split = list(once_id)
   val = 0
   while(len(id_split) > 0):
--- onceid/generators.py
+++ onceid/generators.py
@@ -2,12 +2,15 @@ import random
 
 from onceid import ONCEB50, ONCESIZE
 
+# Generate a random 11-character OnceID.
 def getOnceID(): return getMyOnceID()
 
+# Generate a random 8-character OnceID.
 def getShortOnceID(): return getMyOnceID(8)
 
+# Generate a random n-character OnceID.
 def getMyOnceID(l = 11):
-  if l is None or not isinstance( l, (int, long) ):
+  if not isinstance(l, (int, long)) or l < 3:
     return None
   once_id = ''
   for i in range(l):
